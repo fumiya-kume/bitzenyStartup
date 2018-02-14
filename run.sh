4005232ce82fe29ade26607e41e0579187f2ab95
Fumiya Kume <fumiya.kume@hotmail.com>
2018-02-15 05:01:46 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-cd cpuminer
-./minerd -a yescrypt -o stratum+tcp://bitzeny.bluepool.info:3330 -u ZmE3QxnPc2kbP8D6gik2THfdEw2dsQESTJ
+ ./minerd -a yescrypt -o stratum+tcp://bitzeny.bluepool.info:3330 -u ZmE3QxnPc2kbP8D6gik2THfdEw2dsQESTJ
