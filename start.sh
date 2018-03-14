4435ee9cf9b0e081d37bb55dd07263d91321f835
Fumiya Kume <fumiya.kume@hotmail.com>
2018-03-14 20:50:26 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,6 @@ sudo apt-get install autoconf -y
 sudo apt-get install build-essential libcurl4-openssl-dev -y
 ./autogen.sh    # only needed if building from git repo
 ./nomacro.pl    # in case the assembler doesn't support macros
-./configure CFLAGS="-O3" # Make sure -O3 is an O and not a zero!
+./configure CFLAGS="-O3" -Wcpp # Make sure -O3 is an O and not a zero!
 make
 sh ./run.sh
