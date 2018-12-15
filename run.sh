c87cc4b4cab39157a2c04cffe18c0f6a7315b1f0
Fumiya Kume <fumiya.kume@hotmail.com>
2018-12-16 02:57:37 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-cpuminer/minerd -a yescrypt -o stratum+tcp://bunnymining.work:19333 -u kuxuWorker -p kuxu
+cpuminer/minerd -a yescrypt -o stratum+tcp://bunnymining.work:19333 -u kuxu.kuxuWorker -p kuxu
